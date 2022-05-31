7b2a74639e6a84731b9982c299ec7b0807856149
Mathieu Chartier <mathieuc@google.com>
2022-06-01 00:42:41 +0100

    evergo: Always preopt extracted APKs

--- device.mk
+++ device.mk
@@ -22,6 +22,7 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
