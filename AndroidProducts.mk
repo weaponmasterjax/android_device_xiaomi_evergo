90d081ea514b8f51f11a8bed77262b22974eda99
Taqi Raza <mttalpur@gmail.com>
2022-12-17 15:29:19 +0500

    Sync changes from lineage-19.1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_evergo.mk
+    $(LOCAL_DIR)/arrow_evergo.mk \
+    $(LOCAL_DIR)/lineage_evergo.mk
 
 COMMON_LUNCH_CHOICES := \
     arrow_evergo-user \
     arrow_evergo-userdebug \
-    arrow_evergo-eng
+    arrow_evergo-eng \
+    lineage_evergo-user \
+    lineage_evergo-userdebug \
+    lineage_evergo-eng
---
+++ lineage_evergo.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2022 ArrowOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common.mk)
+
+# Inherit from evergo device
+$(call inherit-product, device/xiaomi/evergo/device.mk)
+
+# Lineage Stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := evergo
+PRODUCT_NAME := lineage_evergo
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 11T 5G
+PRODUCT_MANUFACTURER := xiaomi
+
+# Build info
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="evergo-user 12 SP1A.210812.016 V13.0.5.0.SGBINXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/evergo_in/evergo:12/SP1A.210812.016/V13.0.5.0.SGBINXM:user/release-keys
