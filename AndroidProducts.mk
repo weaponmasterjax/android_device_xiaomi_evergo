308352f3e577c5b0443576714a6e2501a8625508
Taqi Raza <mttalpur@gmail.com>
2022-12-17 16:06:03 +0500

    add aosp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,13 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/arrow_evergo.mk \
+    $(LOCAL_DIR)/aosp_evergo.mk \
     $(LOCAL_DIR)/lineage_evergo.mk
 
 COMMON_LUNCH_CHOICES := \
+    aosp_evergo-user \
+    aosp_evergo-userdebug \
+    aosp_evergo-eng \
     arrow_evergo-user \
     arrow_evergo-userdebug \
     arrow_evergo-eng \
