f793788975554b2a6f6658db8e8c9a848bc7e045
Taqi Raza <mttalpur@gmail.com>
2022-12-23 17:32:11 +0500

    add privapp permissions mtk

--- device.mk
+++ device.mk
@@ -192,6 +192,9 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     ImsServiceBase \
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
+
 # IFAA
 PRODUCT_PACKAGES += \
     IFAAService
