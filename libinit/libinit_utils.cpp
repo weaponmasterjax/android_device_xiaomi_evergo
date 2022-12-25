affa972991381569ec92c0de7550991497dd2b7f
Taqi Raza <mttalpur@gmail.com>
2022-12-25 15:57:14 +0500

    Update libinit
    add fingerprint to flavor

--- libinit/libinit_utils.cpp
+++ libinit/libinit_utils.cpp
@@ -73,3 +73,22 @@ std::string fingerprint_to_description(std::string fingerprint) {
 
     return description;
 }
+
+std::string fingerprint_to_flavor(std::string fingerprint) {
+    std::string delimiter = "/";
+    std::string delimiter2 = ":";
+    std::string build_fingerprint_copy = fingerprint;
+
+    FIND_AND_REMOVE(build_fingerprint_copy, delimiter, brand)
+    FIND_AND_REMOVE(build_fingerprint_copy, delimiter, product)
+    FIND_AND_REMOVE(build_fingerprint_copy, delimiter2, device)
+    FIND_AND_REMOVE(build_fingerprint_copy, delimiter, platform_version)
+    FIND_AND_REMOVE(build_fingerprint_copy, delimiter, build_id)
+    FIND_AND_REMOVE(build_fingerprint_copy, delimiter2, build_number)
+    FIND_AND_REMOVE(build_fingerprint_copy, delimiter, build_variant)
+    std::string build_version_tags = build_fingerprint_copy;
+
+    std::string description = product + "-" + build_variant;
+
+    return description;
+}
