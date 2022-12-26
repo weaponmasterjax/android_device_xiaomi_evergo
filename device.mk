7e028db84e48d12bfbd30b1f998a442b71d4d597
Taqi Raza <mttalpur@gmail.com>
2022-12-26 19:19:34 +0500

    add ims service base

--- device.mk
+++ device.mk
@@ -190,7 +190,7 @@ PRODUCT_BOOT_JARS += \
     mediatek-telephony-common
 
 PRODUCT_PACKAGES += \
-    ImsServiceBase \
+    ImsServiceBase
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
