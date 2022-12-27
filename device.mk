ff981cd35570becad12d87c9cae41a62583bb5d8
Taqi Raza <mttalpur@gmail.com>
2022-12-27 10:47:19 +0500

    Update device.mk
    remove frameworks sensorservice

--- device.mk
+++ device.mk
@@ -401,7 +401,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0.vendor \
     android.hardware.sensors@2.1.vendor \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
-    android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
