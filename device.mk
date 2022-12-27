a6a8a362811a29898ba9bfeb2303f5c42c7021ef
Taqi Raza <mttalpur@gmail.com>
2022-12-27 11:38:14 +0500

    Update device.mk
    drop frameworks sensorservice & libsensor ndk bridge

--- device.mk
+++ device.mk
@@ -394,9 +394,7 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0.vendor \
     android.hardware.sensors@2.0.vendor \
     android.hardware.sensors@2.1.vendor \
-    android.hardware.sensors@2.0-ScopedWakelock.vendor \
-    android.frameworks.sensorservice@1.0.vendor \
-    libsensorndkbridge
+    android.hardware.sensors@2.0-ScopedWakelock.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
