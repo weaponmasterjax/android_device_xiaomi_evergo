1239d3f56b81a81bff7c45057d2f2a8ac41d9e38
Taqi Raza <mttalpur@gmail.com>
2023-01-03 18:50:24 +0500

    add src build oss fp hal

--- device.mk
+++ device.mk
@@ -148,7 +148,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+    android.hardware.biometrics.fingerprint@2.1-service.evergo
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
