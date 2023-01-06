9412c30a11ad515d2274e28f7fd3f294d5c38bd7
Taqi Raza <mttalpur@gmail.com>
2023-01-06 12:35:51 +0500

    Sync changes from a12 branch

--- aosp_evergo.mk
+++ aosp_evergo.mk
@@ -22,12 +22,9 @@ PRODUCT_DEVICE := evergo
 PRODUCT_NAME := aosp_evergo
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11T 5G
-PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
 
 # Build info
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="evergo-user 12 SP1A.210812.016 V13.0.5.0.SGBINXM release-keys"
 
 BUILD_FINGERPRINT := Redmi/evergo_in/evergo:12/SP1A.210812.016/V13.0.5.0.SGBINXM:user/release-keys
