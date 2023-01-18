e806aa2e14d28e8f6349e8c3185094e7bfdf4133
Taqi Raza <mttalpur@gmail.com>
2023-01-18 13:57:59 +0500

    evergo: build sensor service interface

--- device.mk
+++ device.mk
@@ -420,6 +420,7 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0.vendor \
     android.hardware.sensors@2.1.vendor \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
+    android.frameworks.sensorservice@1.0 \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
