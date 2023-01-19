c1bc9f299759f9a050c269172607c6c61b9dd118
Jax Lee <sd369888@outlook.com>
2023-01-19 17:24:05 +0800

    Dotify

--- dot_evergo.mk
+++ dot_evergo.mk
@@ -9,17 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Inherit from evergo device
 $(call inherit-product, device/xiaomi/evergo/device.mk)
 
-# ArrowOS Stuff
+#dot stuffs
+WITH_GMS := true
 TARGET_BOOT_ANIMATION_RES := 1080
+DOT_BUILD_TYPE := UNOFFICIAL
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := evergo
-PRODUCT_NAME := arrow_evergo
+PRODUCT_NAME := dot_evergo
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11T 5G
 PRODUCT_MANUFACTURER := Xiaomi
