aa9c302c7390193e8c6143f6793c6a540cd0a037
Taqi Raza <mttalpur@gmail.com>
2023-01-20 13:25:41 +0500

    evergo: drop old audio hals

--- device.mk
+++ device.mk
@@ -58,18 +58,11 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.common-util.vendor \
-    android.hardware.audio.common@5.0.vendor \
-    android.hardware.audio.common@6.0-util.vendor \
-    android.hardware.audio.common@6.0.vendor \
     android.hardware.audio.common@7.0-util.vendor \
     android.hardware.audio.common@7.0.vendor \
-    android.hardware.audio.effect@6.0-util.vendor \
-    android.hardware.audio.effect@6.0.vendor \
     android.hardware.audio.effect@7.0-util.vendor \
     android.hardware.audio.effect@7.0.vendor \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.audio@6.0.vendor \
-    android.hardware.audio@6.0-util.vendor \
     android.hardware.audio@7.0-util.vendor \
     android.hardware.audio@7.0.vendor \
     audio.primary.default \
