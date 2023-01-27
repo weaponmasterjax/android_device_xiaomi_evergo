2db5355c9367db89390b64aeec2f1769d5e4b2e9
Taqi Raza <mttalpur@gmail.com>
2023-01-27 16:49:30 +0500

    evergo: drop duplicate consumer ir

--- device.mk
+++ device.mk
@@ -95,10 +95,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
-# ConsumerIr
-PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-service.xiaomi
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
