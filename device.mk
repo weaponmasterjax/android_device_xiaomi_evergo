c162f56e7ae1867cd6bafd75a31bf04059c59afb
Jax Lee <sd369888@outlook.com>
2023-02-18 21:59:06 +0800

    Use AOSP audio hal

--- device.mk
+++ device.mk
@@ -55,22 +55,17 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@7.0-impl \
     android.hardware.audio.common-util.vendor \
     android.hardware.audio.common@5.0.vendor \
     android.hardware.audio.common@6.0-util.vendor \
     android.hardware.audio.common@6.0.vendor \
+    android.hardware.audio.common@7.0-enums.vendor \
     android.hardware.audio.common@7.0-util.vendor \
-    android.hardware.audio.common@7.0.vendor \
-    android.hardware.audio.effect@6.0-util.vendor \
-    android.hardware.audio.effect@6.0.vendor \
-    android.hardware.audio.effect@7.0-util.vendor \
     android.hardware.audio.effect@7.0.vendor \
-    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio@6.0.vendor \
-    android.hardware.audio@6.0-util.vendor \
     android.hardware.audio@7.0-util.vendor \
     android.hardware.audio@7.0.vendor \
+    android.hardware.audio.effect@7.0-impl:32
     audio.primary.default \
     audio.r_submix.default \
     audio.usb.default \
